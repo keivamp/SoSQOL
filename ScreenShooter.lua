0f9f0485b3baf6f5638b145190e580dbfeda6151
keivamp <keivamp@live.com>
2021-06-18 01:21:52 -0500

    Add files via upload

---
+++ ScreenShooter.lua
@@ -0,0 +1,41 @@
+--[[
+###########################################
+Symphony of Souls Quality of Life: keivamp#
+###########################################
+]]--
+
+
+
+local _G = _G
+local CreateFrame = _G.CreateFrame
+
+function ScreenShotOnEvent()
+
+	ScreenShotFrame = CreateFrame("Frame")
+	ScreenShotFrame.delay = 1
+	ScreenShotFrame:Show()
+end
+
+function CreateAutoScreenShot()
+
+		 ScreenShotFrame = CreateFrame("Frame")
+		ScreenShotFrame.delay = 1
+	ScreenShotFrame:Show()
+		ScreenShotFrame:SetScript("OnUpdate", function(self, elapsed)
+			self.delay = self.delay - elapsed
+			if self.delay < 0 then
+				Screenshot()
+				self:Hide()
+			end
+		end)
+			
+end
+
+local InkedLoginFrame=CreateFrame("Frame")
+
+		keivampLoginFrame:RegisterEvent("PLAYER_LEVEL_UP",CreateAutoScreenShot)
+		keivampLoginFrame:RegisterEvent("ACHIEVEMENT_EARNED",CreateAutoScreenShot)
+		keivampLoginFrame:SetScript("OnEvent", function(self, event) 
+		CreateAutoScreenShot()
+												
+		end)
